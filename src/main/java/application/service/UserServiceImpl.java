9a37e40d57b83b1057f2e45c0064ea42831c1dec
Solovev Anatoliy <tolya-ptisa@mail.ru>
2021-03-24 13:16:10 +0300

    Initial commit

---
+++ src/main/java/application/service/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package application.service;
+
+import application.dao.UserDao;
+import application.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public void updateUser(Long id, User user) {
+        userDao.updateUser(id, user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userDao.deleteUser(id);
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return null;
+    }
+}
